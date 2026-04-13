58ff21c3860bba5f471fdaa2e853e01b1df096fa
Nabeel Nazeer <nabeel.nazeer13@gmail.com>
2026-04-14 02:46:02 +0530

    confirm database works with test in server

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const app = require('./app.js');
 const connectDB = require('./db/db.js');
+const Highscore = require('./db/highscore');
 
 const PORT = 5080;
 async function startServer() {
     try {      
     await connectDB();
+
+   /* const testScore = new Highscore({
+        name: 'TestPlayer',
+        guesses: 3,
+        time: 45,
+        wordLength: 5,
+        duplicatesAllowed: false,
+  });
+  await testScore.save();
+  console.log('Test score saved:', testScore); */
+
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
@@ -13,3 +25,5 @@ async function startServer() {
     }
 }
 startServer();
+
+
