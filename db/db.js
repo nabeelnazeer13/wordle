6f4b002033d665b256ac39aa082dd09915ad641d
Nabeel Nazeer <nabeel.nazeer13@gmail.com>
2026-04-14 02:44:03 +0530

    fix typo in database and export function

--- db/db.js
+++ db/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/wordle';
+const MONGO_URI = 'mongodb://127.0.0.1:27017/Wordle';
 
-module.exports = {
-  connectDB: async function() {
+module.exports = 
+   async function connectDB() {
     await mongoose.connect(MONGO_URI);
     console.log('MongoDB connected');
-  }
-};
+  };
