5f26f1790acb214cd63ee0fb6ca729c2412a1c04
Nabeel Nazeer <nabeel.nazeer13@gmail.com>
2026-04-13 22:02:18 +0530

    rename folders for better seperation

--- server_db/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const MONGO_URI = 'mongodb://127.0.0.1:27017/wordle';
-
-export async function connectDB() {
-  await mongoose.connect(MONGO_URI);
-  console.log('MongoDB connected');
-}
