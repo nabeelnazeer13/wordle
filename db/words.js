742f640b9c59261a96a81804a2c8ece732073d6d
Nabeel Nazeer <nabeel.nazeer13@gmail.com>
2026-04-15 20:22:49 +0530

    add local word list array

---
+++ db/words.js
@@ -0,0 +1,3 @@
+const words = ['apple', 'grape', 'crane', 'brick', 'flame', 'stone', 'plank', 'glare', 'storm', 'black', 'blaze', 'clamp', 'clash', 'climb', 'clown', 'clump', 'clung', 'clunk', 'clove', 'clone', 'close', 'cloth', 'cloud', 'clout', 'clove', 'clown', 'clubhouse'];
+
+module.exports = words;
