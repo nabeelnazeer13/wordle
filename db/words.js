71462188f632c5a869b369988820575800b88f1c
Nabeel Nazeer <nabeel.nazeer13@gmail.com>
2026-04-16 22:08:37 +0530

    create a cache for wordlist and export it when function is called

--- db/words.js
+++ db/words.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-function loadWords() {
-const words = fs
-  .readFileSync(path.join(__dirname, 'words_alpha.txt'), 'utf-8')
-  .split('\n')
-  .map(w => w.trim().toLowerCase())
-  .filter(w => w.length > 0);
+let words=null;
 
-console.log(`Loaded ${words.length} words`);
+function loadWords() {
+if (!words) {
+    words = fs
+    .readFileSync(path.join(__dirname, 'words_alpha.txt'), 'utf-8')
+    .split('\n')
+    .map(w => w.trim().toLowerCase())
+    .filter(w => w.length > 0);
+    console.log(`Loaded ${words.length} words`);
+}
 return words;
 }
 
