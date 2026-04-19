a004185a8ef003d126ad34715fb63fb3b37349be
Nabeel Nazeer <nabeel.nazeer13@gmail.com>
2026-04-19 11:18:17 +0530

    add INputFeedback component to app.jsx, add wordle logo for styling

--- wordle/src/App.jsx
+++ wordle/src/App.jsx
@@ -2,20 +2,28 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Wordle_Logo from './assets/wordle_logo.png'
 import './App.css'
-import GameSettings from './components/GameSettings';
+import GameSettings from './components/gameSettings';
+import InputFeedback from './components/InputFeedback';
+
 
 function App() {
   const [settings, setSettings] = useState({wordLength: 5,allowDuplicates:false});
+  const [word, setWord] = useState('');
+  const [gameStatus, setGameStatus] = useState('loading');
 
   useEffect(() => {
     const fetchWord = async () => {
       //reset game state, start new game, etc.
+      setGameStatus('loading');
       const { wordLength, allowDuplicates } = settings;
         try {
             const res = await fetch(`/api/word?length=${wordLength}&allowDuplicates=${allowDuplicates}`);
             const data = await res.json();
             console.log('Fetched word:', data.word);
+            setWord(data.word);
+            setGameStatus('playing');
             //set word, game set to playing, reset guess timer, etc.
         } catch (err) {
             console.error('Failed to fetch word:', err);
@@ -26,14 +34,18 @@ function App() {
 
   function handleSettingsChange(newSettings) {
         setSettings(newSettings);
-    }
+  }
+
+  function handleGuessSubmit(feedback) {
+    console.log('Guess submitted! Received feedback:', feedback);
+    //guess array state for visual feedback, check if user win or lose, 
+  }
 
   return (
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={Wordle_Logo} className="base" width="170" height="179" alt="" />
+         
         </div>
         <div>
           <h1>Wordle</h1>
@@ -41,6 +53,15 @@ function App() {
           <p>
             Lets play <code>the wordle</code> game and set the highest score
           </p>
+          {gameStatus === 'loading' && <p>Loading word...</p>}
+        
+          {gameStatus === 'playing' && (
+            <InputFeedback 
+              word={word} 
+              wordLength={settings.wordLength} 
+              onGuessSubmit={handleGuessSubmit} 
+            />
+          )}
         </div>
       </section>
         )
